4720c27dce6497471b15a1e0007751d4976f69b5
Mayya <msalosina@mail.ru>
2023-03-12 22:57:10 +0700

    Stork

--- src/Stork.java
+++ src/Stork.java
@@ -1,2 +1,19 @@
-public class stork {
+/*
+2.Аист
+ */
+
+public class Stork extends Bird implements MakeSound, Fly{
+    public Stork(Integer height, Integer weight, String eyesColor, Integer flyingHeight) {
+        super(height, weight, eyesColor, flyingHeight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Tr-tr-tr-tr-tr-tr-tr-tr-tr-tr-tr-tr-tr");
+    }
+
+    @Override
+    public void fly() {
+        System.out.printf(": Я летаю на высоте %d м\n", 2000);
+    }
 }
