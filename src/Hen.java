a87e44e9ed1afc5c3f2959688579d29d0120977c
Mayya <msalosina@mail.ru>
2023-03-12 22:53:36 +0700

    Hen

--- src/Hen.java
+++ src/Hen.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Hen {
+/*
+1.Курица
+ */
+
+public class Hen extends Bird implements MakeSound, Fly{
+    public Hen(Integer height, Integer weight, String eyesColor, Integer flyingHeight) {
+        super(height, weight, eyesColor, flyingHeight);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ko-ko-ko");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(": Я могу взлететь на забор");
+    }
 }
