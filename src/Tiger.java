b5d9241a37272b638e01d421245ac0b07bec7bf5
Mayya <msalosina@mail.ru>
2023-03-12 22:57:45 +0700

    Tiger

--- src/Tiger.java
+++ src/Tiger.java
@@ -1,2 +1,14 @@
-public class Tiger {
+/*
+2.1 Тигр
+ */
+
+public class Tiger extends WildAnimal implements MakeSound{
+    public Tiger(Integer height, Integer weight, String eyesColor, String location, String dateOfFinding){
+        super(height, weight, eyesColor, location, dateOfFinding);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Hrrrrrrrrrrrrrr");
+    }
 }
