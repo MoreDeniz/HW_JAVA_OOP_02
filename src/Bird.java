e81d4b05d9328aee2c48aac0bdc6ee29c1449be2
Mayya <msalosina@mail.ru>
2023-03-12 22:46:42 +0700

    Bird

--- src/Bird.java
+++ src/Bird.java
@@ -1,2 +1,30 @@
-public class Bird {
+/*
+реализовать абстрактный класс "Птица", реализующий интерфейс животное,
+ у которого будет защищённое поле:
+1.Высота полёта
+и метод:
+2. Летать, выводящий на экран фразу "Я лечу на x метрах, где x - высота полёта
+и защищённый конструктор:
+3.Конструктор принимающий высоту полёта
+И следующие классы-наследники:
+1.Курица
+2.Аист
+ */
+
+public abstract class Bird extends Animal implements Fly, MakeSound{
+    private Integer flyingHeight;
+
+    public Bird(Integer height, Integer weight, String eyesColor, Integer flyingHeight) {
+        super(height, weight, eyesColor);
+        this.flyingHeight = flyingHeight;
+    }
+
+//    public Integer getFlyingHeight() {
+//        return flyingHeight;
+//    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "; Высота полёта: " + flyingHeight + " м";
+    }
 }
