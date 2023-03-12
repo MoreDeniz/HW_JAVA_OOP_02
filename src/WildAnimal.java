26aded8ff02d45142468ba7cf1358c525f2c04c1
Mayya <msalosina@mail.ru>
2023-03-12 22:58:55 +0700

    Wild Animal

--- src/WildAnimal.java
+++ src/WildAnimal.java
@@ -1,2 +1,28 @@
-public class WildAnimal {
+/*
+2.Дикое животное со следующими свойствами:
+    2.1 Место обитания - location
+    2.2 Дата нахождения - dateOfFinding
+ */
+
+public abstract class WildAnimal extends Animal implements MakeSound{
+    private String location;
+    private String dateOfFinding;
+
+    public WildAnimal(Integer height, Integer weight, String eyesColor,
+                      String location, String dateOfFinding) {
+        super(height, weight, eyesColor);
+        this.location = location;
+        this.dateOfFinding = dateOfFinding;
+    }
+
+    String getLocation(){
+        return location;
+    }
+    String getDateOfFinding(){
+        return dateOfFinding;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + "; Location: " + getLocation() + "; Day of finding: " + getDateOfFinding();
+    }
 }
