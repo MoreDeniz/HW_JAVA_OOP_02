0946feeaa7f6cd335a5acc97d105e5a3089e7afb
Mayya <msalosina@mail.ru>
2023-03-12 22:59:22 +0700

    Wolf

--- src/Wolf.java
+++ src/Wolf.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Wolf {
+/*
+4.Волк со свойством:
+    4.1 Вожак стаи(доступным только для чтения внешним классам)
+ */
+
+public class Wolf extends WildAnimal implements MakeSound{
+    private String leader;
+    public Wolf(Integer height, Integer weight, String eyesColor,
+                String location, String dateOfFinding, String leader) {
+        super(height, weight, eyesColor, location, dateOfFinding);
+        this.leader = leader;
+    }
+
+    String getLeader(){
+        return leader;
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Wououuuuu!");
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "; Is leader: " + getLeader();
+    }
 }
